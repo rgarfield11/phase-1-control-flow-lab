fccbcd5f49870186d4aab8bdac823f145b587e37
Ross <rossgarfield@gmail.com>
2022-04-25 17:26:02 -0500

    commit tree 29c9560fec9aedfffa93366e8e88e52a30b9de98

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distanceInFeet) {
   // Write your code here!
+  if (distanceInFeet <= 400) {
+    return 'This one is on me!';
+  } else if (distanceInFeet <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (distanceInFeet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
+} 
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  return (city === 'NYC' ? 'Ok, sounds good.' : 'No go.');
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    default:
+      return 'Bye.';
+  }
 }
